6b6ba68f13778a86c849ed7a67abe517e70be22e
Linnea Ternevik <linnea.ternevik@gmail.com>
2024-01-07 11:31:13 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -66,18 +66,19 @@ def update_worksheet(data, worksheet):
     print(f"{worksheet} worksheet updated successfully\n")
 
 
-def calculate_surplus_data(sales_row):
-    """
+def calculate_surplus_data(sales_row): 
+    """ 
     Compare sales with stock and calculate the surplus for each item type.
 
     The surplus is defined as the sales figure subtracted from the stock:
     - Positive surplus indicates waste
     - Negative surplus indicates extra made when stock was sold out.
     """
+    
     print("calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    
+
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
@@ -86,6 +87,23 @@ def calculate_surplus_data(sales_row):
     return surplus_data
     
    
+def get_last_5_entries_sales():
+    """
+    Collects collumns of data from sales worksheet, collecting 
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+  
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+   
+
+
 def main():
     """
     Run all program functions
@@ -97,6 +115,7 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+#main()
 
 
+sales_columns = get_last_5_entries_sales()
